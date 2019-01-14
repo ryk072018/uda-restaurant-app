38fecddf5996518d3b6d27346cf8e0b21bcb7cf3
ryk01 <ryk2100@163.com>
2019-01-14 23:45:15 +0800

    add viewport

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWFwYm94LXJ5azAxIiwiYSI6ImNqcXZ5anA1ODEwYmgzeHB1NnlmY2lzbGoifQ.EynSR3ANks_8NCucOjoocQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -138,7 +138,7 @@ resetRestaurants = (restaurants) => {
     self.markers.forEach(marker => marker.remove());
   }
   self.markers = [];
-  self.restaurants = restaurants;
+  self.restaurants = restaurants; 
 }
 
 /**
